abcf94391aca86c9637d772cc268ea71f42afab1
Wizard-G <wizg.main@gmail.com>
2015-05-17 17:42:59 +0900

    Upgrade to Bootstrap 3.3.4 compatible

--- common.inc.php
+++ common.inc.php
@@ -32,8 +32,8 @@ require_once("snappy/autoload.php");
 
 define("COLOR_INPUT_NUM", 7);
 
-define("OUTPUT_VERSION", "14");
-define("BOOTSTRAP_VERSION", "3.3.2");
+define("OUTPUT_VERSION", "15");
+define("BOOTSTRAP_VERSION", "3.3.4");
 
 $GLOBALS["valid_preview_designs"] = array(
 	"default" => "preview-default.html",
@@ -90,18 +90,18 @@ function connect_db() {
 function check_url($url, $extension = null) {
 	if (!preg_match('/^(https?|ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)$/', $url)) {
 		return false;
-	} 
-	
+	}
+
 	if (substr($url, 0, strlen(BASE_URL)) !== BASE_URL) {
 		return false;
 	}
-	
+
 	if (!is_null($extension)) {
 		if (substr($url, -strlen($extension)) !== $extension) {
 			return false;
 		}
 	}
-	
+
 	return true;
 }
 
@@ -114,13 +114,13 @@ function check_colors($req) {
 	else if (!isset($GLOBALS["api_type_list"][$api_type])) {
 		return false;
 	}
-	
+
 	// id
 	$id = $req->params("id");
 	if (!is_string($id) || $id == "") {
 		return false;
 	}
-		
+
 	// c
 	$c = $req->params("c");
 	if (!is_array($c)) {
@@ -129,7 +129,7 @@ function check_colors($req) {
 	else if (count($c) != COLOR_INPUT_NUM) {
 		return false;
 	}
-	
+
 	foreach ($c as $key => $val) {
 		if (!ctype_digit((string)$key) || $key < 0 || $key >= COLOR_INPUT_NUM) {
 			return false;
@@ -139,14 +139,14 @@ function check_colors($req) {
 			return false;
 		}
 	}
-	
+
 	return true;
 }
 
 function get_style_path_parts($api_type, $id, $c) {
 	ksort($c);
 	$ret = $api_type . "/" . substr($c[0], 0, 2) . "/" . $id . "-" . implode("-", $c) . "/";
-	return $ret; 
+	return $ret;
 }
 
 function fix_directory_separater($value, $ds = null) {
@@ -192,28 +192,28 @@ function generate($api_type, $id, $c, $preview = false) {
 	}
 
 	$path_less = "../../../../less/";
-	
+
 	$path_parts = get_style_path_parts($api_type, $id, $c);
 	$path_parts_md5_8 = substr(md5($path_parts), 0, 8);
-	
+
 	$path_output = dirname(__FILE__) . "/output/" . OUTPUT_VERSION . "/" . $path_parts;
-	
+
 	$path_bootstrap_css = $path_output . "bootstrap.css";
 	$path_bootstrap_min_css = $path_output . "bootstrap.min.css";
 	$path_bootstrap_less = $path_output . "bootstrap.less";
 	$path_variables_less = $path_output . "variables.less";
 	$path_zip = $path_output . "paintstrap-" . $api_type . "-" . $id . "-" . $path_parts_md5_8 . ".zip";
 	$path_zip_kickstrap = $path_output . "paintstrap-k-" . $api_type . "-" . $id . "-" . $path_parts_md5_8 . ".zip";
-	
+
 	$url_output = BASE_URL . "output/" . OUTPUT_VERSION . "/" . $path_parts;
-	
+
 	$url_bootstrap_css = $url_output . "bootstrap.css";
 	$url_bootstrap_min_css = $url_output . "bootstrap.min.css";
 	$url_variables_less = $url_output . "variables.less";
 	$url_zip = $url_output . basename($path_zip);
 	$url_zip_kickstrap = $url_output . basename($path_zip_kickstrap);
-	
-	if (!USE_LESSC_CACHE || 
+
+	if (!USE_LESSC_CACHE ||
 			!file_exists($path_bootstrap_min_css) ||
 			!file_exists($path_bootstrap_less))
 	{
@@ -234,14 +234,15 @@ function generate($api_type, $id, $c, $preview = false) {
 		$output = array();
 		//error_log($command);
 		exec($command, $output);
+		//error_log(implode(" / ", $output));
 	}
-		
+
 	if (!$preview) {
 		ob_start();
 		require(dirname(__FILE__) . "/less/variables.less");
 		$variables_less = ob_get_clean();
-		
-		if (!USE_LESSC_CACHE || 
+
+		if (!USE_LESSC_CACHE ||
 				!file_exists($path_variables_less) ||
 				!file_exists($path_bootstrap_css) ||
 				!file_exists($path_zip) ||
@@ -254,11 +255,11 @@ function generate($api_type, $id, $c, $preview = false) {
 			$output = array();
 			exec($command, $output);
 
-			$command = sprintf(COMMAND_ZIP, 
-				escapeshellarg(basename($path_zip)), 
+			$command = sprintf(COMMAND_ZIP,
+				escapeshellarg(basename($path_zip)),
 				implode(" ", array(
-					escapeshellarg(basename($path_bootstrap_css)), 
-					escapeshellarg(basename($path_bootstrap_min_css)), 
+					escapeshellarg(basename($path_bootstrap_css)),
+					escapeshellarg(basename($path_bootstrap_min_css)),
 					escapeshellarg(basename($path_variables_less))
 				))
 			);
@@ -266,13 +267,13 @@ function generate($api_type, $id, $c, $preview = false) {
 			chdir($path_output);
 			$output = array();
 			exec($command, $output);
-			
+
 			$path_kickstrap_theme = "themes/" . $api_result["themeDirName"] . "/";
 			chdir($path_output);
 			@mkdir($path_kickstrap_theme, 0777, true);
 			copy($path_variables_less, $path_output . $path_kickstrap_theme . basename($path_variables_less));
-			$command = sprintf(COMMAND_ZIP_KICKSTRAP, 
-				escapeshellarg(basename($path_zip_kickstrap)), 
+			$command = sprintf(COMMAND_ZIP_KICKSTRAP,
+				escapeshellarg(basename($path_zip_kickstrap)),
 				"themes"
 			);
 			$command = fix_directory_separater($command);
@@ -280,7 +281,7 @@ function generate($api_type, $id, $c, $preview = false) {
 			exec($command, $output);
 		}
 	}
-	
+
 	$ret = array(
 		"url_bootstrap_css" => $url_bootstrap_css,
 		"url_bootstrap_min_css" => $url_bootstrap_min_css,
@@ -296,7 +297,7 @@ function generate($api_type, $id, $c, $preview = false) {
 function get_thumbnail_path($theme_id) {
 	$theme_id_md5_2 = substr(md5($theme_id), 0, 2);
 	$path_image = "gallery/output/" . $theme_id_md5_2 . "/" . $theme_id . ".png";
-	return $path_image;	
+	return $path_image;
 }
 
 function generate_for_gallery($theme_id) {
@@ -304,21 +305,21 @@ function generate_for_gallery($theme_id) {
 	if (is_null($theme)) {
 		return false;
 	}
-	
+
 	if ($theme["share"] != 1) {
 		return false;
 	}
-	
+
 	$path_image = BASE_PATH . get_thumbnail_path($theme_id);
 	if (!file_exists(dirname($path_image))) {
 		mkdir(dirname($path_image), 0777, true);
 	}
-	
+
 	if (!file_exists($path_image)) {
 		$snappy = new Image(COMMAND_WKHTMLTOIMAGE, array(
-			"format" => "png", 
-			"width" => 200, 
-			"zoom" => 0.75, 
+			"format" => "png",
+			"width" => 200,
+			"zoom" => 0.75,
 			"quality" => 50,
 			"disable-javascript" => true
 		));
@@ -333,10 +334,10 @@ function generate_for_gallery($theme_id) {
 		$url = str_replace("%5B", "[", $url);
 		$url = str_replace("%5D", "]", $url);
 		$image = $snappy->getOutput($url);
-	
+
 		file_put_contents($path_image, $image);
 	}
-	
+
 	return true;
 }
 
@@ -345,13 +346,13 @@ function generate_by_id($theme_id) {
 	if (is_null($theme)) {
 		return null;
 	}
-	
+
 	return generate($theme["api_type"], $theme["cs_id"], $theme["_c"]);
 }
 
 function find_theme($theme_id) {
 	$dbh = connect_db();
-	
+
 	$sql = "select * from themes where theme_id = ? and share = 1";
 	$stmt = $dbh->prepare($sql);
 	$stmt->bindValue(1, $theme_id);
@@ -360,12 +361,12 @@ function find_theme($theme_id) {
 	if (empty($row)) {
 		return null;
 	}
-	
+
 	$row["_c"] = explode(",", $row["colors"]);
 	if (count($row["_c"]) != COLOR_INPUT_NUM) {
 		return null;
 	}
-	
+
 	return $row;
 }
 
@@ -374,14 +375,14 @@ function save_theme($api_type, $id, $c, $share) {
 	if (!$api_result || $api_result["record"] == 0) {
 		return false;
 	}
-	
+
 	$now = date("Y-m-d H:i:s");
-	
+
 	ksort($c);
 	$colors = implode(",", $c);
-	
+
 	$save_taggings = false;
-	
+
 	$dbh = connect_db();
 
 	$dbh->beginTransaction();
@@ -395,7 +396,7 @@ function save_theme($api_type, $id, $c, $share) {
 		$row = $stmt->fetch(PDO::FETCH_ASSOC);
 		if (!empty($row)) {
 			$theme_id = $row["theme_id"];
-			
+
 			if ($row["share"] == 0 && $share) {
 				$sql = "update themes set cs_name = ?, share = 1, ip_addr = ?, created_at = now(), updated_at = now() where theme_id = ?";
 				$stmt = $dbh->prepare($sql);
@@ -417,9 +418,9 @@ function save_theme($api_type, $id, $c, $share) {
 			$stmt->bindValue(5, ($share ? 1 : 0));
 			$stmt->bindValue(6, $_SERVER["REMOTE_ADDR"]);
 			$stmt->execute();
-			
-			$theme_id = $dbh->lastInsertId(); 
-			
+
+			$theme_id = $dbh->lastInsertId();
+
 			if ($share) {
 				$save_taggings = true;
 			}
@@ -433,7 +434,7 @@ function save_theme($api_type, $id, $c, $share) {
 
 			$tagging_tag_names = calculate_taggings($c);
 			$tags = find_tags();
-			
+
 			$sql = "insert into taggings (theme_id, tag_id, created_at) values (?, ?, now())";
 			$stmt = $dbh->prepare($sql);
 			foreach ($tagging_tag_names as $tagging_tag_name) {
@@ -441,7 +442,7 @@ function save_theme($api_type, $id, $c, $share) {
 				if ($tag_id === false) {
 					throw new Exception("Tag not found");
 				}
-	
+
 				$stmt->bindValue(1, $theme_id);
 				$stmt->bindValue(2, $tag_id);
 				$stmt->execute();
@@ -460,14 +461,14 @@ function save_theme($api_type, $id, $c, $share) {
 
 function add_download_count($theme_id) {
 	$dbh = connect_db();
-	
+
 	$theme = find_theme($theme_id);
 	if (is_null($theme)) {
 		return null;
 	}
 
-	if ($theme["last_download_ip_addr"] == "" || 
-			(string)$theme["last_download_ip_addr"] !== (string)$_SERVER["REMOTE_ADDR"]) 
+	if ($theme["last_download_ip_addr"] == "" ||
+			(string)$theme["last_download_ip_addr"] !== (string)$_SERVER["REMOTE_ADDR"])
 	{
 		$sql = "update themes set download_count = download_count + 1, last_download_ip_addr = ? where theme_id = ?";
 		$stmt = $dbh->prepare($sql);
@@ -475,7 +476,7 @@ function add_download_count($theme_id) {
 		$stmt->bindValue(2, $theme_id);
 		$stmt->execute();
 	}
-	
+
 	return true;
 }
 
@@ -504,7 +505,7 @@ function find_tags() {
 
 function get_tag_ids($tag_names) {
 	$tags = find_tags();
-	
+
 	$ret = array();
 	foreach ($tag_names as $key => $tag_name) {
 		$idx = array_search($tag_name, $tags);
@@ -517,7 +518,7 @@ function get_tag_ids($tag_names) {
 
 function find_themes($api_types, $cs_id, $tag_ids, $limit = null, $offset = null, $count_only = false) {
 	$dbh = connect_db();
-	
+
 	$cs_id_where = "";
 	if ($cs_id != "") {
 		$cs_id_where = " and cs_id = " . $dbh->quote($cs_id) . " ";
@@ -531,7 +532,7 @@ function find_themes($api_types, $cs_id, $tag_ids, $limit = null, $offset = null
 		}
 		$api_type_where = " and api_type in (" . implode(",", $quoted_api_types) . ") ";
 	}
-	
+
 	if ($count_only) {
 		$sql = "select count(*) as c from themes where " . _make_find_themes_where($tag_ids) .
 				$cs_id_where .
@@ -543,20 +544,20 @@ function find_themes($api_types, $cs_id, $tag_ids, $limit = null, $offset = null
 	else {
 		$sql = "select * from themes where " . _make_find_themes_where($tag_ids) .
 				$cs_id_where .
-				$api_type_where . 
+				$api_type_where .
 				" order by created_at desc limit " . $limit . " offset " . $offset;
 		$stmt = $dbh->prepare($sql);
 		$stmt->execute();
 		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		
+
 		foreach ($rows as $key => $row) {
 			$rows[$key]["_url_thumbnail"] = BASE_URL . get_thumbnail_path($row["theme_id"]);
-			
+
 			$created_at_utc = new DateTime($rows[$key]["created_at"]);
 			$created_at_utc->setTimezone(new DateTimeZone('UTC'));
 			$rows[$key]["_created_at_utc"] = $created_at_utc;
 		}
-			
+
 		return $rows;
 	}
 }
@@ -565,22 +566,22 @@ function _make_find_themes_where($tag_ids) {
 	$dbh = connect_db();
 
 	$wheres = array();
-	
+
 	if (count($tag_ids) > 0) {
 		$sql = "select theme_id from taggings where tag_id = ?";
 		$stmt = $dbh->prepare($sql);
-		
+
 		$theme_ids = array();
 		foreach ($tag_ids as $tag_id) {
 			$tmp_theme_ids = array();
-			
+
 			$stmt->bindValue(1, $tag_id);
 			$stmt->execute();
 			$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 			foreach ($rows as $row) {
 				$tmp_theme_ids[] = $row["theme_id"];
 			}
-			
+
 			if (count($theme_ids) == 0) {
 				$theme_ids = $tmp_theme_ids;
 			}
@@ -588,7 +589,7 @@ function _make_find_themes_where($tag_ids) {
 				$theme_ids = array_intersect($theme_ids, $tmp_theme_ids);
 			}
 		}
-		
+
 		if (count($theme_ids) == 0) {
 			$wheres[] = "false";
 		}
@@ -596,9 +597,9 @@ function _make_find_themes_where($tag_ids) {
 			$wheres[] = "theme_id in (" . implode(",", $theme_ids) . ")";
 		}
 	}
-	
+
 	$wheres[] = "share = 1";
-	
+
 	$where = implode(" and ", $wheres);
 	return $where;
 }
@@ -631,9 +632,9 @@ function call_kuler_api($kuler_id) {
 	if (!ctype_digit($kuler_id)) {
 		return null;
 	}
-	
+
 	$cache_lite = create_cache_lite_object();
-	
+
 	$xml_txt = $cache_lite->get($kuler_id, "kuler_api");
 	if (!USE_COLOR_SCHEME_API_CACHE || !$xml_txt) {
 		$params = array(
@@ -645,15 +646,15 @@ function call_kuler_api($kuler_id) {
 		if (!$xml_txt) {
 			return null;
 		}
-	
+
 		$xml_txt = str_replace("kuler:", "kuler_", $xml_txt);
 		$xml_txt = str_replace("xmlns:", "xmlns_", $xml_txt);
-		
+
 		$xml = @simplexml_load_string($xml_txt);
 		if (!$xml || !isset($xml->channel->recordCount)) {
 			return null;
 		}
-		
+
 		if ($xml->channel->recordCount > 0) {
 			$cache_lite->save($xml_txt, $kuler_id, "kuler_api");
 		}
@@ -661,19 +662,19 @@ function call_kuler_api($kuler_id) {
 	else {
 		$xml = @simplexml_load_string($xml_txt);
 	}
-	
+
 	if ($xml->channel->recordCount == 0) {
 		$ret = array(
 			"apiType" => "kuler",
 			"record" => 0
 		);
 	}
-	else {	
+	else {
 		$hex = array();
 		foreach ($xml->channel->item->kuler_themeItem->kuler_themeSwatches->kuler_swatch as $key => $val) {
 			$hex[] = (string)$val->kuler_swatchHexColor;
 		}
-		
+
 		$ret = array(
 			"apiType" => "kuler",
 			"record" => 1,
@@ -692,9 +693,9 @@ function call_colourlovers_api($palette_id) {
 	if (!ctype_digit($palette_id)) {
 		return null;
 	}
-	
+
 	$cache_lite = create_cache_lite_object();
-		
+
 	$xml_txt = $cache_lite->get($palette_id, "colourlovers_api");
 	if (!USE_COLOR_SCHEME_API_CACHE || !$xml_txt) {
 		$url = COLOURLOVERS_API_URL . "palette/" . $palette_id;
@@ -702,7 +703,7 @@ function call_colourlovers_api($palette_id) {
 		if (!$xml_txt) {
 			return null;
 		}
-		
+
 		$xml = @simplexml_load_string($xml_txt);
 		if (!$xml) {
 			// do nothing
@@ -721,12 +722,12 @@ function call_colourlovers_api($palette_id) {
 			"record" => 0
 		);
 	}
-	else {	
+	else {
 		$hex = array();
 		foreach ($xml->palette->colors->hex as $key => $val) {
 			$hex[] = (string)$val;
 		}
-		
+
 		$ret = array(
 			"apiType" => "colourlovers",
 			"record" => 1,
@@ -738,18 +739,18 @@ function call_colourlovers_api($palette_id) {
 			"themeDirName" => make_theme_dir_name((string)$xml->palette->title)
 		);
 	}
-	
+
 	return $ret;
 }
 
 function calculate_taggings($colors) {
 	$target_colors = array($colors[4], $colors[5]);
-	
+
 	$tagging_tag_names = array();
-	
+
 	foreach ($target_colors as $color) {
 		$color_obj = new Color("#" . $color);
-			
+
 		$hsl = $color_obj->getHsl();
 
 		if ($hsl["L"] > 0.9) {
@@ -795,7 +796,7 @@ function calculate_taggings($colors) {
 				$tagging_tag_names[] = "purple";
 				break;
 		}
-		
+
 		if ($hsl["S"] > (2.0 / 3.0)) {
 			$tagging_tag_names[] = "high_saturation";
 		}
@@ -805,7 +806,7 @@ function calculate_taggings($colors) {
 		else {
 			$tagging_tag_names[] = "normal_saturation";
 		}
-		
+
 		if ($hsl["L"] > (2.0 / 3.0)) {
 			$tagging_tag_names[] = "high_lightness";
 		}
