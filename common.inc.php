8a296e59f98b99b48a04ce565872ce797e77895b
Wizard-G <wizg.main@gmail.com>
2015-07-11 17:24:35 +0900

    Upgrade to Bootstrap 3.3.5 compatible

--- common.inc.php
+++ common.inc.php
@@ -32,8 +32,8 @@ require_once("snappy/autoload.php");
 
 define("COLOR_INPUT_NUM", 7);
 
-define("OUTPUT_VERSION", "15");
-define("BOOTSTRAP_VERSION", "3.3.4");
+define("OUTPUT_VERSION", "16");
+define("BOOTSTRAP_VERSION", "3.3.5");
 
 $GLOBALS["valid_preview_designs"] = array(
 	"default" => "preview-default.html",
